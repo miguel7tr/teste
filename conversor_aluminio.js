564745f1b81220a84cde62ba4b52f2abc5c1f6b0
miguel_t_rodrigues <miguel_t_rodrigues@estudante.sesisenai.org.br>
2024-02-07 16:03:10 -0300

    documentação

--- conversor_aluminio.js
+++ conversor_aluminio.js
@@ -1,4 +1,4 @@
-let resistividade = 0.00172 //resistividade condutor do cobre 0,0172 Ω. mm²/m
+let resistividade = 0.0282 //resistividade condutor do alumínio 0,0282 Ω. mm²/m
 
 let comprimento = 1555
 let areaTransversal = 2.5 //o valor mínimo é 1 para não dividir por zero
